153209b86e39e6c3f878e77a7cdfcaf8893138ab
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2024-09-27 14:48:21 -0300

    BD usuarios creada OK

---
+++ PruebaDrawer/app/src/main/java/Entidades/Usuario.java
@@ -0,0 +1,38 @@
+package Entidades;
+
+public class Usuario {
+
+    private String nombreUsuario;
+    private String emailUsuario;
+    private String passwordUsuario;
+
+    public Usuario(String nombreUsuario, String emailUsuario, String passwordUsuario) {
+        this.nombreUsuario = nombreUsuario;
+        this.emailUsuario = emailUsuario;
+        this.passwordUsuario = passwordUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getPasswordUsuario() {
+        return passwordUsuario;
+    }
+
+    public void setPasswordUsuario(String passwordUsuario) {
+        this.passwordUsuario = passwordUsuario;
+    }
+}
